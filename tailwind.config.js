06fcacbd6d28b9194957c7e4f752063262ab1ba0
Ariful islam <afkgamingno1@gmail.com>
2024-10-31 19:28:48 +0600

    add header and hero section all content

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+
+// module.exports = {
+//     content: [
+//       "./src/**/*.{html,js}",  
+//       "./public/index.html",   
+//     ],
+//     theme: {
+//       extend: {
+       
+//       },
+//     },
+//     plugins: [
+//       require("daisyUi"), 
+//     ],
+//     daisyui: {
+//       themes: ["light"], 
+//     },
+//   }
+  
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
